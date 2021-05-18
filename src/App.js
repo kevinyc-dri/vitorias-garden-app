66d0fca33724b7edc55228addcdaf41a6184e101
Kevin Choi <kevinyoungchoi@gmail.com>
2021-05-18 17:04:13 -0400

    created new user section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css'
 
 import Login from './scenes/Login'
 
-import Main from './components/Main'
 import Footer from './components/Footer'
 import Nav from './components/Nav'
 import Orders from './scenes/Orders'
--- src/scenes/Users.js
+++ src/scenes/Users.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button } from 'react-materialize'
 
 function Users() {
+  const [allUsers, setAllUsers] = useState()
+  const [newUser, setNewUser] = useState({})
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then((response) => response.json())
+      .then((data) => setAllUsers(data))
+      .catch((err) => console.log('This is an error', err))
+  }, [])
+
+  function createUser() {
+    fetch('http://localhost:5000/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+  }
+
   return (
-    <p>orders</p>
+    <div>
+      <ul class="collection with-header">
+        <h1>this is users</h1>
+        {allUsers && allUsers.map((user) => {
+          return (
+            <li class="collection-item">
+              <div>
+                {user.email} / {user.firstName} {user.lastName}
+                <a href="#!" class="secondary-content">
+                  <i class="material-icons">send</i>
+                </a>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+      <label>Email</label>
+      <input onChange={e => setNewUser({ ...newUser, email: e.target.value })} />
+      <label>Password</label>
+      <input onChange={e => setNewUser({ ...newUser, password: e.target.value })} />
+      <Button onClick={() => createUser()}>Create User</Button>
+    </div>
   )
 }
 
